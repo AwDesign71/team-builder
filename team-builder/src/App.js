91c553efdc845456583d3d10ece262cee12c5b46
React Learning <amwilson2019@gmail.com>
2019-07-24 18:23:14 -0400

    Just test the onChange

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,12 +1,39 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
-import Teammembers from './components/TeamMembers';
+import Members from './teamData';
+
 function App() {
+  const [teams, setTeams] = useState(Members)
+  const handleValue = (event)=> {
+    setTeams(event.target.value);
+    console.log(teams)
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <Teammembers />
+        { console.log(teams)}
+        {/* <h1>{teams.map(team=>
+          <div>{team.name}</div>)}</h1> */}
       </header>
+      <section>
+      <form action="">
+        <legend>
+          <label htmlFor="">
+            Member Name:
+            <input type="text" name={handleValue.name} onChange={event=>handleValue(event)} />
+          </label>
+          <label htmlFor="">
+            Member Email:
+            <input type="text" name='email'/>
+          </label>
+          <label htmlFor="">
+            Member Role:
+            <input type="text" name='role'/>
+          </label>
+          
+        </legend>
+      </form>
+      </section>
     </div>
   );
 }
